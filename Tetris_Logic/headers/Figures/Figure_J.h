b6db76a0e1e46735fb463740392ada69442e09d8
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-09-09 19:14:31 +0200

    figure J

---
+++ Tetris_Logic/headers/Figures/Figure_J.h
@@ -0,0 +1,31 @@
+//
+// Created by Przemek on 09.09.2020.
+//
+
+#ifndef TETRIS_FIGURE_J_H
+#define TETRIS_FIGURE_J_H
+
+#include "Figure.h"
+
+
+class Figure_J : public Figure {
+public:
+Figure_J();
+
+~Figure_J();
+
+private:
+std::vector<std::vector<std::shared_ptr<Field>>> baseShape = {
+        {std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(false)),
+                std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(false))},
+        {std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(true)),
+                std::make_shared<Field>(new Field(true)),  std::make_shared<Field>(new Field(false))},
+        {std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(true)),
+                std::make_shared<Field>(new Field(true)),  std::make_shared<Field>(new Field(false))},
+        {std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(false)),
+                std::make_shared<Field>(new Field(false)), std::make_shared<Field>(new Field(false))}
+};
+};
+
+
+#endif //TETRIS_FIGURE_J_H
---
+++ Tetris_Logic/src/Figures/Figure_J.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Przemek on 09.09.2020.
+//
+
+#include "Figures/Figure_J.h"
+
+
+Figure_J::Figure_J() : Figure() {
+    this->shape = baseShape;
+}
+
+Figure_J::~Figure_J() {
+
+}
