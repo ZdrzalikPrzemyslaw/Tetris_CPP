ebe485117ecc4f86f69aede70033c15289be0174
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-09-09 14:42:08 +0200

    update gitignore

---
+++ cmake-build-debug/CMakeFiles/Tetris.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Studia/Tetris/Tetris_Logic/src/Board.cpp" "D:/Studia/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/Tetris_Logic/src/Board.cpp.obj"
+  "D:/Studia/Tetris/main.cpp" "D:/Studia/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/Tetris_Logic/src/Board.cpp.obj"
+  "CMakeFiles/Tetris.dir/main.cpp.obj"
+  "Tetris.exe"
+  "Tetris.exe.manifest"
+  "Tetris.pdb"
+  "libTetris.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
