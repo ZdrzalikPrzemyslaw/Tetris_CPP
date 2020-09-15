91c617322c45d1ba3908cb5066ebba500ad29077
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-09-15 21:19:05 +0200

    Del

--- Game/src/TextureManager.cpp
+++ Game/src/TextureManager.cpp
@@ -8,7 +8,6 @@
 SDL_Texture *TextureManager::loadTexture(const char *texture, SDL_Renderer *renderer) {
     SDL_Surface* tempSurface = IMG_Load(texture);
     SDL_Texture* sdlTexture = SDL_CreateTextureFromSurface(renderer, tempSurface);
-    printf("IMG_Load: %s\n", IMG_GetError());
     SDL_FreeSurface(tempSurface);
     return sdlTexture;
 }
