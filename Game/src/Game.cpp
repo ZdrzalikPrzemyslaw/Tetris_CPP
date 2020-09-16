081add458ea759a876ecce50ac3836b4be38f0cb
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-09-16 21:50:05 +0200

    fix window icon

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -39,8 +39,7 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
     this->board = new Board();
     this->tetrisDisplay = TetrisDisplay(this->renderer, this->board);
     this->lastTetrisDownMove = SDL_GetTicks();
-    auto surface = IMG_Load("assets/icon.bmp");
-    SDL_SetWindowIcon(this->window, surface);
+    SDL_SetWindowIcon(this->window, IMG_Load("assets/icon.bmp"));
 }
 
 Game::Game() {
